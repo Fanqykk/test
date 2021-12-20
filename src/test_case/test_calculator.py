d4c3714e594aa52adf6b8249769b669fe2e1e3c1
Fanqykk <fqy_yk@163.com>
2021-12-20 15:56:13 +0800

    修改路径

--- src/test_case/test_calculator.py
+++ src/test_case/test_calculator.py
@@ -56,7 +56,7 @@ class MyTests(unittest.TestCase):
         except Exception as e:
             print("Test fail.", format(e))
             # 截图路保存径，绝对路径，也可以用相对路径
-            screenshoturl = r"D:\code\git\test\src\report\images\\"
+            screenshoturl = r"C:\ProgramData\Jenkins\.jenkins\workspace\python_autotest\src\report\images\\"
             # 时间样式
             timestrmap = time.strftime('%Y%m%d_%H.%M.%S')
             # 寻找失败时自动截图至指定目录images，截图名称为 时间戳 + png后缀
